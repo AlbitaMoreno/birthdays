9f7728d8126259b474e063e3a5c4924157f6bd6a
Alba <albamoreno.ontiveros@gmail.com>
2018-07-01 19:46:15 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.get("/", function (req, res, next) {
       name,
       birth_date,
       EXTRACT(YEAR FROM age(birth_date)) AS age,
-      EXTRACT('day' FROM birth_date) AS day_b,
-      EXTRACT('month' FROM birth_date) AS month_b,
+      EXTRACT(DAY FROM birth_date) AS day_b,
+      EXTRACT(MONTH FROM birth_date) AS month_b,
       (day_b || '-' || month_b ) as fecha
   FROM
       "People"
