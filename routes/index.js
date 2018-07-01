6a8b291ae7f98085e261cc5ed1261eb562088761
Alba <albamoreno.ontiveros@gmail.com>
2018-07-01 19:36:38 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res, next) {
   FROM
       "People"
   ORDER BY
-      fecha ASC `,
+      ABS(DATEDIFF(DATE(fecha), CURDATE())) DESC `,
       {
         model: models.Person
       }
