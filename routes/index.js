8b20312c0487b436450e64fff0535d2c9a25a1a2
Alba <albamoreno.ontiveros@gmail.com>
2018-07-01 20:28:45 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,13 @@ router.get("/", function (req, res, next) {
       name,
       birth_date,
       EXTRACT(YEAR FROM age(birth_date)) AS age,
-      (EXTRACT(MONTH FROM birth_date) || '-' || EXTRACT(DAY FROM birth_date)) as fecha
+      (EXTRACT(MONTH FROM birth_date) || '-' || EXTRACT(DAY FROM birth_date)) as fecha,
+      (EXTRACT(MONTH FROM now()) || '-' || EXTRACT(DAY FROM now())) as fecha_n
   FROM
       "People"
   ORDER BY
-      fecha DESC `,
+      fecha < fecha_n DESC,
+      fecha > fecha_n ASC `,
       {
         model: models.Person
       }
