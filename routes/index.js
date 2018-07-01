0d7e9a0d4a5538f5fa1ab8ca37249be961e4b18f
Alba <albamoreno.ontiveros@gmail.com>
2018-07-01 19:22:02 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,23 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   models.sequelize
     .query(
       `SELECT
-          name,
-          birth_date,
-          EXTRACT(YEAR FROM age(birth_date)) AS age
-      FROM
-          "People"
-      ORDER BY
-          birth_date DESC`,
+      name,
+      birth_date,
+      EXTRACT(YEAR FROM age(birth_date)) AS age,
+      (EXTRACT('day' FROM birth_date) || '-' || EXTRACT('month' FROM birth_date)) as fecha
+  FROM
+      "People"
+  ORDER BY
+      fecha DESC`,
       {
         model: models.Person
       }
     )
-    .then(function(people) {
+    .then(function (people) {
       res.render("index", { title: "Celebrities, ordered by age", people: people });
     });
 });
